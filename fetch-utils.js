72bd01d0c7d2985f12bb1ae8f5e945bd9da87d74
Steven Bryant <stevelad_83@yahoo.com>
2022-11-14 18:02:39 -0500

    added supabase url and key

---
+++ fetch-utils.js
@@ -0,0 +1,5 @@
+const SUPABASE_URL = 'https://yhtqjrmxupxpmxmjrqpo.supabase.co';
+const SUPABASE_KEY =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlodHFqcm14dXB4cG14bWpycXBvIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjgxMDgyNTQsImV4cCI6MTk4MzY4NDI1NH0.Bp6AKfoL4GrrPbg0WYAwHMhKSaKZFGfEYwaewQNhY4M';
+
+const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
